22d32b290383ef5638b2c7cdfd517b253c430a73
Raza Mahmood <razammahmood@gmail.com>
2024-02-27 21:15:42 -0500

    convert uofg-alert

--- src/components/uofg-alert/uofg-alert.tsx
+++ src/components/uofg-alert/uofg-alert.tsx
@@ -2,22 +2,22 @@ import { Component, h } from '@stencil/core';
 import { FontAwesomeIcon } from 'utils/font-awesome-icon';
 import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
 
-@Component({ tag: 'uofg-alert', styleUrl: 'uofg-alert.scss', shadow: true })
+@Component({ tag: 'uofg-alert', styleUrl: 'uofg-alert.css', shadow: true })
 export class UofgAlert {
   render() {
     return (
-      <div id="uofg-alert">
-        <div id="uofg-alert-title">
+      <div class="tw-flex tw-flex-col tw-text-3xl">
+        <div class="tw-flex tw-items-center tw-bg-uofg-red tw-p-8 tw-text-4xl tw-text-white [&>svg]:tw-mr-4 [&>svg]:tw-h-[1.5em] [&>svg]:tw-fill-current">
           <FontAwesomeIcon icon={faCircleExclamation} />
           <slot name="title"></slot>
         </div>
 
-        <div id="uofg-alert-body">
+        <div class='tw-flex tw-flex-col tw-bg-white tw-px-8 tw-py-6 [&>slot[name="message"]::slotted(*)]:tw-text-2xl [&>slot[name="subtitle"]::slotted(*)]:tw-mb-8 [&>slot[name="subtitle"]::slotted(*)]:tw-text-4xl [&>slot[name="subtitle"]::slotted(*)]:tw-font-bold'>
           <slot name="subtitle"></slot>
           <slot name="message"></slot>
         </div>
 
-        <div id="uofg-alert-footer">
+        <div class="tw-flex tw-bg-uofg-grey tw-px-4 tw-py-8 tw-text-2xl">
           <slot name="footer"></slot>
         </div>
       </div>
