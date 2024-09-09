7b33dc105679b29623c9d0641ceb9fca6f42d9c9
Raza Mahmood <razammahmood@gmail.com>
2024-09-09 15:55:17 -0400

    refactor uofg-footer to spilt source into separate components

---
+++ src/components/uofg-footer/address.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { faPhoneFlip } from '@fortawesome/free-solid-svg-icons';
+  import FontAwesomeIcon from '../../lib/font-awesome-icon.svelte';
+  import { address } from './data/guelph.js';
+</script>
+
+<address class="flex flex-col justify-between gap-4 not-italic">
+  <strong>{address.title}</strong>
+  <span>{address.street},</span>
+  <span>{address.city}, Ontario, Canada</span>
+  <span>{address.postalCode}</span>
+  <a
+    href={`tel:${address.phoneNumber}`}
+    class="flex justify-center items-center w-fit gap-2 border-0 border-b-2 border-dotted border-transparent text-uofg-blue transition-colors focus:border-current hover:border-current"
+  >
+    <FontAwesomeIcon icon={faPhoneFlip} />
+    <span>{address.phoneNumber}</span>
+  </a>
+</address>
---
+++ src/components/uofg-footer/social.svelte
@@ -0,0 +1,21 @@
+<script>
+  import FontAwesomeIcon from '../../lib/font-awesome-icon.svelte';
+  import { social } from './data/guelph.js';
+</script>
+
+<ul class="flex gap-3 text-4xl [&>li]:contents">
+  {#each social.links as { href, text, icon, classes }}
+    <li>
+      <a {href} aria-label={text} class={`transition-colors ${classes}`}>
+        <FontAwesomeIcon {icon} />
+      </a>
+    </li>
+  {/each}
+</ul>
+
+<a
+  class="w-fit border-0 border-b-2 border-dotted border-transparent transition-colors focus:border-white hover:border-white"
+  href={social.directory}
+>
+  Social Media Directory
+</a>
