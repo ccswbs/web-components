38e36604925444a1099e0086766295da4704c3fc
Raza Mahmood <razammahmood@gmail.com>
2024-09-09 15:48:50 -0400

    fix import

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './styles/global.css';
-import UofGHeader from './components/uofg-header.svelte';
+import UofGHeader from './components/uofg-header/uofg-header.svelte';
 import UofGFooter from './components/uofg-footer.svelte';
 import UofGModal from './components/uofg-modal.svelte';
 import UofGAlert from './components/uofg-alert.svelte';
