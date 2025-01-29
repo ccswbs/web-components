5e2b7566c7a944333816713960669dd31e21c4af
Raza Mahmood <razammahmood@gmail.com>
2025-01-29 11:22:31 -0500

    Add color variants and Storybook examples for uofg-alert

--- src/components/uofg-alert.svelte
+++ src/components/uofg-alert.svelte
@@ -1,6 +1,9 @@
 <svelte:options
   customElement={{
     tag: 'uofg-alert',
+    props: {
+      color: { reflect: true, type: 'String', attribute: 'color' },
+    },
     extend: customElementConstructor => {
       return class extends customElementConstructor {
         constructor() {
@@ -16,24 +19,32 @@
   import attachTailwind from '../lib/attach-tailwind.js';
   import FontAwesomeIcon from '../lib/font-awesome-icon.svelte';
   import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
+
+  let { color = 'red' } = $props();
 </script>
 
 <div class="flex flex-col">
   <div
-    class="flex items-center bg-red p-4 text-lg text-white [&>svg]:mr-4 [&>svg]:h-[1.5em] [&>svg]:fill-current"
+    class="flex items-center p-4 text-lg [&>svg]:mr-4 [&>svg]:h-[1.5em] [&>svg]:fill-current"
+    class:bg-red={color === 'red'}
+    class:bg-yellow={color === 'yellow'}
+    class:bg-green={color === 'green'}
+    class:bg-blue={color === 'blue'}
+    class:text-white={color === 'red' || color === 'blue' || color === 'green'}
   >
     <FontAwesomeIcon icon={faCircleExclamation} />
     <slot name="title" />
   </div>
 
   <div
-    class={`flex flex-col bg-white px-6 py-3 [&>slot[name="message"]::slotted(*)]:text-base [&>slot[name="subtitle"]::slotted(*)]:mb-4 [&>slot[name="subtitle"]::slotted(*)]:text-xl [&>slot[name="subtitle"]::slotted(*)]:font-bold`}
+    class={`flex flex-col bg-white px-6 py-3 [&>slot[name="message"]::slotted(*)]:text-base [&>slot[name="subtitle"]::slotted(*)]:mb-4 [&>slot[name="subtitle"]::slotted(*)]:text-xl [&>slot[name="subtitle"]::slotted(*)]:font-bold border-light-grey border-t-0 border`}
+    class:border-b={!$$slots?.footer}
   >
     <slot name="subtitle" />
     <slot name="message" />
   </div>
 
-  <div class="flex bg-light-grey px-4 py-2 text-lg">
+  <div class="flex bg-light-grey text-lg" class:px-4={$$slots?.footer} class:py-2={$$slots?.footer}>
     <slot name="footer" />
   </div>
 </div>
---
+++ src/stories/components/alert.stories.svelte
@@ -0,0 +1,64 @@
+<script context="module">
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import '../../components/uofg-alert.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Components/Alert',
+    component: 'uofg-alert',
+    parameters: {
+      layout: 'padded',
+    },
+  });
+</script>
+
+<Story name="Basic">
+  <uofg-alert>
+    <span slot="title">YOUR TITLE HERE</span>
+    <span slot="message">YOUR ALERT MESSAGE HERE</span>
+  </uofg-alert>
+</Story>
+
+<Story name="With Subtitle">
+  <uofg-alert>
+    <span slot="title">YOUR TITLE HERE</span>
+    <span slot="message">YOUR ALERT MESSAGE HERE</span>
+    <span slot="subtitle">YOUR SUBTITLE HERE</span>
+  </uofg-alert>
+</Story>
+
+<Story name="With Footer">
+  <uofg-alert>
+    <span slot="title">YOUR TITLE HERE</span>
+    <span slot="message">YOUR ALERT MESSAGE HERE</span>
+    <span slot="subtitle">YOUR SUBTITLE HERE</span>
+    <span slot="footer">YOUR FOOTER CONTENT HERE</span>
+  </uofg-alert>
+</Story>
+
+
+<Story name="Yellow">
+  <uofg-alert color="yellow">
+    <span slot="title">YOUR TITLE HERE</span>
+    <span slot="subtitle">YOUR SUBTITLE HERE</span>
+    <span slot="message">YOUR ALERT MESSAGE HERE</span>
+    <span slot="footer">YOUR FOOTER CONTENT HERE</span>
+  </uofg-alert>
+</Story>
+
+<Story name="Blue">
+  <uofg-alert color="blue">
+    <span slot="title">YOUR TITLE HERE</span>
+    <span slot="subtitle">YOUR SUBTITLE HERE</span>
+    <span slot="message">YOUR ALERT MESSAGE HERE</span>
+    <span slot="footer">YOUR FOOTER CONTENT HERE</span>
+  </uofg-alert>
+</Story>
+
+<Story name="Green">
+  <uofg-alert color="green">
+    <span slot="title">YOUR TITLE HERE</span>
+    <span slot="subtitle">YOUR SUBTITLE HERE</span>
+    <span slot="message">YOUR ALERT MESSAGE HERE</span>
+    <span slot="footer">YOUR FOOTER CONTENT HERE</span>
+  </uofg-alert>
+</Story>
