0858e813f8d8e29350dd259dfe6ed5427468b7c2
Raza Mahmood <razammahmood@gmail.com>
2025-01-31 12:29:44 -0500

    Add BackToTop stories and documentation NEEDS WORK

---
+++ src/stories/components/back-to-top.stories.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import '../../components/uofg-back-to-top.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Components/BackToTop',
+    component: 'uofg-back-to-top',
+    parameters: {
+      layout: 'padded',
+    },
+  });
+</script>
+
+<Story name="Basic">
+  <div style="height: 1000px"></div>
+
+  <uofg-back-to-top></uofg-back-to-top>
+</Story>
